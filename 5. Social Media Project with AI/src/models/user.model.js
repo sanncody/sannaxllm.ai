df9499412df63c8a291e7b4f34bbafa344d64a7f
Sandeep <sandeepwadhawan2001@gmail.com>
2025-11-16 21:42:25 +0530

    Started with auth routes

--- 5. Social Media Project with AI/src/models/user.model.js
+++ 5. Social Media Project with AI/src/models/user.model.js
@@ -1,10 +1,25 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    name: String,
-    email: String,
-    password: String,
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String
+    },
+    profile: {
+        type: String
+    },
     posts: [
         {
             type: mongoose.Schema.Types.ObjectId,
--- 5. Social Media Project with AI/src/routes/auth.routes.js
+++ 5. Social Media Project with AI/src/routes/auth.routes.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const userModel = require('../models/user.model');
 
 const router = express.Router();
 
-router.get('/testing', (_, res) => {
-    res.json({ test: "This is just a testing route" });
+/**
+ * POST /register
+ * POST /login
+ * GET /user [protected]
+ */
+
+router.post('/register', async (req, res) => {
+    const { username, name, email, password } = req.body;
+
+    const userExists = await userModel.findOne({ username });
+
+    if (userExists) {
+        return res.status(409).json({ message: "Username already in use" });
+    }
+
+    const user = await userModel.create({
+        username,
+        name,
+        email,
+        password
+    });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+    res.cookie('token', token);
+
+    res.status(201).json({ message: "User registered successfully", user });
 });
 
 module.exports = router;

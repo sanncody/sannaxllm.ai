320905ff29b6cf1e8b906b6a14bc3083085b31c5
Sandeep <sandeepwadhawan2001@gmail.com>
2025-12-17 23:57:27 +0530

    Listening an event on backend for sending message in a particular chat via chatId

--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -1,20 +1,41 @@
 const { Server } = require('socket.io');
 const cookie = require('cookie');
+const jwt = require('jsonwebtoken');
 
 const app = require('../app');
+const userModel = require('../models/user.model');
 
 const initSocketServer = (httpServer) => {
     const io = new Server(httpServer, {});
 
-    // Use Socket.io Middleware just to ensure that only logged in user can create socket connection
-    io.use((socket, next) => {
-        const cookies = cookie.parse(socket.handshake.headers.cookie);
+    // Use "Socket.io Middleware" just to ensure that only logged in user can create socket.io connection
+    io.use(async (socket, next) => {
+        const cookies = cookie.parse(socket.handshake.headers.cookie || "");
         console.log(cookies);
         
+        // If token not found then pass the request further with an error
+        if (!cookies.token) {
+            next(new Error("Authentication Error: No token provided"));
+        }
+
+        try {
+            // If token found, then verify the token
+            const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET_KEY);
+
+            const user = await userModel.findOne({ _id: decoded.id });
+
+            socket.user = user;
+
+            next();
+        } catch (error) {
+            next(new Error("Authentication error: Invalid token"));
+        }
     });
 
     io.on('connection', (socket) => {
-        console.log("New socket connection", socket.id);
+        socket.on('ai-message', async (messagePayload) => {
+            console.log(messagePayload);
+        });
     });
 };
 
